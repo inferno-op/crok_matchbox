35942c1f036c307bbe0346af4afca85355a69629
inferno-op <ivar@inferno-op.com>
2014-03-16 14:54:13 +0100

    Added Matte IN / OUTPUT option

--- crok_pixelate/crok_pixelate.glsl
+++ crok_pixelate/crok_pixelate.glsl
@@ -1,4 +1,5 @@
 uniform sampler2D image;
+uniform sampler2D alpha;
 uniform float adsk_result_w, adsk_result_h;
 uniform float Detail;
 
@@ -8,5 +9,8 @@ void main(void)
 {
 	vec2 uv = gl_FragCoord.xy / iResolution.xy;
 	vec2 pixel = floor( uv * Detail );
-	gl_FragColor = texture2D(image, pixel / Detail );
+	vec4 pixel_img = vec4( texture2D(image, pixel / Detail ).rgb * texture2D(alpha, pixel / Detail).rgb, texture2D(alpha, pixel / Detail).b);
+	vec4 normal_img = vec4( texture2D(image, uv));
+
+	gl_FragColor = vec4 ( mix(normal_img.rgba, pixel_img.rgba, 1.0));
 }
