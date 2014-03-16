3e78c413a47b5d0125a9f9c860e925d2ef84a610
inferno-op <ivar@inferno-op.com>
2014-03-16 23:46:05 +0100

    fixed 'matte out' issue (the shader didn't supplied a working output matte)
    now you are able to comp directly inside the shader and output a matte
    for the effect as well

--- crok_pixelate/crok_pixelate.glsl
+++ crok_pixelate/crok_pixelate.glsl
@@ -2,6 +2,7 @@ uniform sampler2D image;
 uniform sampler2D alpha;
 uniform float adsk_result_w, adsk_result_h;
 uniform float Detail;
+uniform float trans;
 
 vec2 iResolution = vec2(adsk_result_w, adsk_result_h);
 
@@ -9,6 +10,8 @@ void main(void)
 {
 	vec2 uv = gl_FragCoord.xy / iResolution.xy;
 	vec2 pixel = floor( uv * Detail );
-	gl_FragColor = texture2D(alpha, pixel / Detail) * (texture2D(image, pixel / Detail )) + (1.0 - texture2D(alpha, pixel / Detail)) * (texture2D(image, uv ));
+	vec3 color1 = vec3( texture2D(image, pixel / Detail ).rgb);	
+	vec3 color2 = vec3( texture2D(image, uv).rgb);
+	vec3 matte =  vec3( texture2D(alpha, pixel / Detail).rgb);
+	gl_FragColor = vec4 ((matte * color1 + (1.0 - matte) *color2) , matte);
 }
-
