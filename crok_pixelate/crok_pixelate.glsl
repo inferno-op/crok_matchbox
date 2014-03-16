50f8f17acf43a29a93e0b886aeeb7f9779bb7480
inferno-op <ivar@inferno-op.com>
2014-03-17 00:30:27 +0100

    added the possibility to blend in the pixelation effect

--- crok_pixelate/crok_pixelate.glsl
+++ crok_pixelate/crok_pixelate.glsl
@@ -8,10 +8,11 @@ vec2 iResolution = vec2(adsk_result_w, adsk_result_h);
 
 void main(void)
 {
+//	vec2 uv = gl_FragCoord.xy / iResolution.xy;
 	vec2 uv = gl_FragCoord.xy / iResolution.xy;
 	vec2 pixel = floor( uv * Detail );
 	vec3 color1 = vec3( texture2D(image, pixel / Detail ).rgb);	
 	vec3 color2 = vec3( texture2D(image, uv).rgb);
 	vec3 matte =  vec3( texture2D(alpha, pixel / Detail).rgb);
-	gl_FragColor = vec4 ((matte * color1 + (1.0 - matte) *color2) , matte);
+	gl_FragColor = vec4 ((matte * color1*trans + (1.0 - matte*trans) *color2) , matte);
 }
