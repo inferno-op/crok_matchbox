24e61a9fa456ee5435cfce51f2983a336282fddb
inferno-op <ivar@inferno-op.com>
2014-05-18 15:32:20 +0200

    added AMOUNT slider
    you can now blend in the BW effect

--- crok_bw/crok_bw.glsl
+++ crok_bw/crok_bw.glsl
@@ -3,22 +3,22 @@ vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 
 uniform sampler2D Source;
 uniform float Exposure;
+uniform float amount;
+
 uniform float Red;
 uniform float Green;
 uniform float Blue;
 
 vec3 RGB_lum = vec3(Red, Green, Blue);
-
 const vec3 lumcoeff = vec3(0.2126,0.7152,0.0722);
 
 void main (void) 
 { 		
 	vec2 uv = gl_FragCoord.xy / resolution.xy;
 	vec4 tc = texture2D(Source, uv);
-	tc = tc * (exp2(tc)*vec4(Exposure));
+	vec4 tc_new = tc * (exp2(tc)*vec4(Exposure));
 	vec4 RGB_lum = vec4(lumcoeff * RGB_lum, 0.0 );
-	float lum = dot(tc,RGB_lum);
+	float lum = dot(tc_new,RGB_lum);
 	vec4 luma = vec4(lum);
-	gl_FragColor = vec4(luma.rgb,1.0);
-
+	gl_FragColor = mix(tc, luma, amount);
 } 
