179a65b704f78258aecd61842e860c63a8f096a9
inferno-op <ivar@inferno-op.com>
2014-05-29 12:32:43 +0200

    submit to GitHub

---
+++ crok_new_balance/crok_new_balance.glsl
@@ -0,0 +1,39 @@
+uniform float adsk_result_w, adsk_result_h;
+vec2 res = vec2(adsk_result_w, adsk_result_h);
+
+uniform sampler2D Front;
+uniform float level;
+uniform int colourspace;
+uniform int Computing;
+
+#extension GL_ARB_shader_texture_lod : enable
+
+void main(void)
+{
+vec2 st = gl_FragCoord.xy / res;
+
+vec4 front = texture2D(Front, st);
+vec4 front_a = texture2DLod(Front, st, level - 0.5);
+
+   if( colourspace == 0)  // Rec 709 input is selected
+	{  
+	  if( Computing == 0)
+	  {
+		  front /= 2.0 * front_a;
+	  }
+	   if( Computing == 1)
+		   front-=front_a+0.244459-0.918031;
+
+	   if( Computing == 2)
+		   front-=front_a-(front_a[0]+front_a[1]+front_a[2])/3.0;
+	   
+//	   pow(front,vec4(.4545));
+   }
+   
+   if( colourspace == 1)  // Linear input is selected
+	   front /= 2.0 * front_a;
+
+
+   
+gl_FragColor = front;
+}
