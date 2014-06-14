84901eaea8611433c960671b296c8fdff0aa8db1
inferno-op <ivar@inferno-op.com>
2014-06-14 19:57:34 +0200

    code cleanup + aspect slider added
    - now the checkerboard is square by default
    - added aspect ratio slider
    - did some code cleanup

--- crok_checkerboard/crok_checkerboard.glsl
+++ crok_checkerboard/crok_checkerboard.glsl
@@ -1,12 +1,15 @@
-uniform float adsk_result_w, adsk_result_h, Number;
+uniform float adsk_result_w, adsk_result_h, Zoom, Aspect;
 uniform vec3 color1, color2;
 vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 vec3 checkerboard(vec2 p, float freq, vec3 first, vec3 second)
 {
-	return 	mix(first, second, max(0.0, sign(sin(p.x * 6.283*freq)) * sign(sin(p.y * 6.283 * freq))));
+	return 	mix(first, second, max(0.0, sign(sin(p.x * freq * Aspect * (adsk_result_w / adsk_result_h))) * sign(sin(p.y * freq ))));
 }
 void main( void ) {
-	vec2 p = ( gl_FragCoord.xy / resolution.xy );
-	p.x *= resolution.x / resolution.y;
-	gl_FragColor = vec4(checkerboard( p, Number, vec3(color1.r, color1.g, color1.b), vec3(color2.r, color2.g, color2.b)), 1.0 );
+
+	vec2 p = (gl_FragCoord.xy / resolution.xy) ;
+	p=(2.0*(p-.5));
+
+	gl_FragColor = vec4(checkerboard( p, Zoom, color1, color2), 1.0 );
+
 }
