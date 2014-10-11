09c1ec60e8c00dacc5abba5fe6af96c22e6b47b3
inferno-op <ivar@inferno-op.com>
2014-10-11 14:39:43 +0200

    changed the way the softness scales

--- crok_regrain/crok_regrain.03.glsl
+++ crok_regrain/crok_regrain.03.glsl
@@ -1,10 +1,11 @@
 uniform sampler2D adsk_results_pass2;
-uniform float blur, adsk_result_w, adsk_result_h;
+uniform float blur, size, adsk_result_w, adsk_result_h;
 
 void main()
 {
    vec2 coords = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
-   int f0int = int(blur);
+   float softness = blur * size;
+   int f0int = int(softness);
    vec4 accu = vec4(0);
    float energy = 0.0;
    vec4 finalColor = vec4(0.0);
@@ -13,7 +14,7 @@ void main()
    {
       vec2 currentCoord = vec2(coords.x, coords.y+float(y)/adsk_result_h);
       vec4 aSample = texture2D(adsk_results_pass2, currentCoord).rgba;
-      float anEnergy = 1.0 - ( abs(float(y)) / blur);
+      float anEnergy = 1.0 - ( abs(float(y)) / softness);
       energy += anEnergy;
       accu+= aSample * anEnergy;
    }
