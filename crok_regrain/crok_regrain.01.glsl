4e820ce143a3ddf0c1e3beee977e30efeefe4aee
inferno-op <ivar@inferno-op.com>
2014-10-11 14:28:54 +0200

    added Grain Size and Grain Aspect slider

--- crok_regrain/crok_regrain.01.glsl
+++ crok_regrain/crok_regrain.01.glsl
@@ -7,7 +7,7 @@ vec2 res = vec2(adsk_result_w, adsk_result_h);
 float time = adsk_time *.05;
 
 uniform bool color_noise;
-uniform float amount_r, amount_g, amount_b, overall;
+uniform float amount_r, amount_g, amount_b, overall, size, aspect;
 
 uniform int stock;
 
@@ -17,13 +17,16 @@ float rand2(vec2 co)
 	return fract(sin(dot(co.xy,vec2(12.9898,78.233))) * 43758.5453);
 }
 
-vec3 noise(vec2 st) {
-	vec2 c = (100.*res.x)*vec2(1.,res.y/res.x);
+vec3 noise(vec2 uv) {
+	vec2 c = ((1./size)*res.x)*vec2(1.,(res.y/res.x));
 	vec3 col = vec3(0.0);
-
-   	float r = rand2(vec2((2.+time) * floor(st.x*c.x)/c.x, (2.+time) * floor(st.y*c.y)/c.y ));
-   	float g = rand2(vec2((5.+time) * floor(st.x*c.x)/c.x, (5.+time) * floor(st.y*c.y)/c.y ));
-   	float b = rand2(vec2((9.+time) * floor(st.x*c.x)/c.x, (9.+time) * floor(st.y*c.y)/c.y ));
+	if ( aspect < 1.0 )
+		uv.x = uv.x / aspect;
+	else 
+		uv.y = uv.y * aspect;
+   	float r = rand2(vec2((2.+time) * floor(uv.x*c.x)/c.x, (2.+time) * floor(uv.y*c.y)/c.y ));
+   	float g = rand2(vec2((5.+time) * floor(uv.x*c.x)/c.x, (5.+time) * floor(uv.y*c.y)/c.y ));
+   	float b = rand2(vec2((9.+time) * floor(uv.x*c.x)/c.x, (9.+time) * floor(uv.y*c.y)/c.y ));
 
 	col = vec3(r,g,b);
 
