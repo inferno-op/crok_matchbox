08a1c3f90c1cd20ddf11b56179e6f18a3283d792
inferno-op <ivar@inferno-op.com>
2014-11-02 17:23:48 +0100

    added organic mode switch
    Thx Vico… i totally ignored the multiply thing…
    Hope this is the look you would expect.

--- crok_vignette/crok_vignette.glsl
+++ crok_vignette/crok_vignette.glsl
@@ -4,20 +4,30 @@ vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 
 uniform float radius, softness, blend;
 uniform vec3 v_color;
+uniform bool organic;
 
 void main( void ) 
 {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
 	vec2 center = 2.0 * ((gl_FragCoord.xy / resolution.xy) - 0.5);
 	vec3 original = texture2D(Source, uv).rgb;
-	vec3 col = v_color * original;
+	vec3 tint_col = v_color * original;
 	
     float length = length(center);
     float vig = smoothstep(radius, radius-softness, length);
 	
 	vec3 matte = vec3(1.0-vig);
-	vec3 fin_col = matte * col + (1.0 - matte) * original;
-	fin_col = mix(original, fin_col, blend);
+
+	// simple blend mode 
+	vec3 fin_col = mix(original, tint_col, blend);
+	fin_col = matte * fin_col + (1.0 - matte) * original;
 	
+	if ( organic )
+	{
+		fin_col = tint_col * original;
+		fin_col = matte * fin_col + (1.0 - matte) * original;
+		fin_col = mix(original, fin_col, blend);
+	}
+
 	gl_FragColor = vec4(fin_col, vig);
 }
