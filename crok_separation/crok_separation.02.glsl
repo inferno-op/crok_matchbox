f51560ab0c6a415b1f4adc1ea66aa5731641d7aa
inferno-op <ivar@inferno-op.com>
2014-11-16 14:09:27 +0100

    submitted to GitHub

---
+++ crok_separation/crok_separation.02.glsl
@@ -0,0 +1,28 @@
+#version 120
+
+uniform float blur_fg, adsk_result_w, adsk_result_h;
+uniform sampler2D adsk_results_pass1;
+
+void main()
+{
+   vec2 coords = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
+   int f0int = int(blur_fg);
+   vec4 accu = vec4(0);
+   float energy = 0.0;
+   vec4 blur_fg_y = vec4(0.0);
+   
+   for( int y = -f0int; y <= f0int; y++)
+   {
+      vec2 currentCoord = vec2(coords.x, coords.y+float(y)/adsk_result_h);
+      vec4 aSample = texture2D(adsk_results_pass1, currentCoord).rgba;
+      float anEnergy = 1.0 - ( abs(float(y)) / blur_fg);
+      energy += anEnergy;
+      accu+= aSample * anEnergy;
+   }
+   
+   blur_fg_y = 
+      energy > 0.0 ? (accu / energy) : 
+                     texture2D(adsk_results_pass1, coords).rgba;
+                     
+   gl_FragColor = vec4( blur_fg_y );
+}
---
+++ crok_separation/crok_separation.03.glsl
@@ -0,0 +1,55 @@
+#version 120
+
+uniform sampler2D source, adsk_results_pass2;
+uniform float adsk_result_w, adsk_result_h;
+vec2 resolution = vec2(adsk_result_w, adsk_result_h);
+uniform float contrast;
+uniform bool toggle;
+
+
+vec3 linearLight( vec3 s, vec3 d )
+{
+	return 2.0 * s + d - 1.0;
+}
+
+vec3 subtract( vec3 s, vec3 d )
+{
+	return s - d;
+}
+
+vec3 linearDodge( vec3 s, vec3 d )
+{
+	return s + d;
+}
+
+
+void main(void)
+{
+	vec2 uv = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h);
+	
+	// source
+	vec3 scr = texture2D(source, uv).rgb;
+	
+	// blured source
+	vec3 b_scr = texture2D(adsk_results_pass2, uv).rgb;
+	
+	vec3 c = vec3(0.0);
+	vec3 fifty_p_grey = vec3(0.5);
+	
+	// suptract operation
+	vec3 sub =  c + subtract(scr, b_scr);
+	
+	// linear dodge operation
+	c =  c + linearDodge(sub, fifty_p_grey);
+	
+	// adjust contrast to 50%
+	c = c * 0.5 + 0.25;	
+	
+	// merge highpass ontop of the blured painted one
+	//c =  linearLight(c, b_scr);
+	
+	gl_FragColor = vec4(b_scr, c);
+	
+	if ( toggle )
+		gl_FragColor = vec4(c, c);
+}
