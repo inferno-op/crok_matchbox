ca4397a3b23c5c4ff4ac76cd6487c7818e681819
inferno-op <ivar@inferno-op.com>
2014-12-16 22:20:11 +0100

    Added CLAMP button

--- crok_lava/crok_lava.glsl
+++ crok_lava/crok_lava.glsl
@@ -8,6 +8,7 @@ uniform float brightness;
 uniform float contrast;
 uniform float saturation;
 uniform float tint;
+uniform bool clamp;
 uniform vec3 co0, co1, co2, co3, co4;
 uniform vec3 tint_col;
 uniform vec2 Aspect;
@@ -146,5 +147,7 @@ void main(void)
 	vec3 brt_color = con_color - 1.0 + brightness;
 	vec3 fin_color = mix(brt_color, brt_color * tint_col, tint);
 
+	if ( clamp )
+		fin_color = clamp(fin_color, 0.0, 1.0);
     gl_FragColor = vec4(fin_color, 1.0);
 	}
