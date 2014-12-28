ac52b9ecc476d000422297b2877bac306be3b6d6
inferno-op <ivar@inferno-op.com>
2014-12-28 15:02:39 +0100

    changed rotation to Degrees input

--- crok_checkerboard/crok_checkerboard.glsl
+++ crok_checkerboard/crok_checkerboard.glsl
@@ -1,37 +1,38 @@
 #version 120
-// based on https://www.shadertoy.com/view/Md2XWt
 
-uniform float adsk_result_w, adsk_result_h, adsk_result_frameratio, Zoom, Aspect;
-vec2 resolution = vec2(adsk_result_w, adsk_result_h);
-
+uniform float adsk_result_w, adsk_result_h, adsk_result_frameratio;
 uniform float rot;
 uniform float zoom;
 uniform float blur;
+uniform float Aspect;
 uniform vec3 color1, color2;
+#define PI 3.14159265359
+
+vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 
     
 void main(void)
 {
-	vec2 uv = (gl_FragCoord.xy / resolution.xy) - 0.5;
-	uv.x /= Aspect;
-	vec2 div = vec2( zoom, zoom * resolution.y/resolution.x );
+	vec2 uv = ((gl_FragCoord.xy / resolution.xy) - 0.5) * zoom;
+	uv.x *= adsk_result_frameratio / Aspect;
 	float bl = 0.0;
 
 	if ( rot != 0.0 )
 		bl += blur; 
 
 	float b = bl * zoom / resolution.x;
-    float st = sin(rot);
-    float ct = cos(rot);
-    vec2 xy0 = div* uv;
-    vec2 xy;
-    xy.x =  ct*xy0.x + st*xy0.y;
-    xy.y = -st*xy0.x + ct*xy0.y;
-    vec2 sxy = sin(3.14159*xy);
-	vec2 square = smoothstep( -b, b, sxy );
+
+	// degrees to radians conversion
+	float rad_rot = rot * PI / 180.0; 
+	
+	// rotation
+	mat2 rotation = mat2( cos(-rad_rot), -sin(-rad_rot), sin(-rad_rot), cos(-rad_rot));
+	uv *= rotation;
+	
+    vec2 anti_a = sin(PI * uv);
+	vec2 square = smoothstep( -b, b, anti_a );
 	square = 2.0 * square - 1.0;						
     float a = 0.5 * (square.x * square.y) + 0.5;
-	vec3 aa = vec3(a) * color1;
 	vec3 c = mix(color1, color2, a); 
 	gl_FragColor = vec4(c, a);
 }
