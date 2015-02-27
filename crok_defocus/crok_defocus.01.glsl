35288900c3302bb606a06ea5bfae7fcd1e58379e
inferno-op <ivar@inferno-op.com>
2015-02-27 19:03:22 +0100

    FIXED Premultiplied issue
    Now you can blur the FG without getting black edges

--- crok_defocus/crok_defocus.01.glsl
+++ crok_defocus/crok_defocus.01.glsl
@@ -6,6 +6,8 @@ uniform sampler2D front, matte;
 void main()
 {
    vec2 uv = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
-   
-   gl_FragColor = clamp(vec4( texture2D(front, uv).rgb, texture2D(matte, uv).b ), 0.0, 10000000000.0);
+   vec3 col = clamp(vec3( texture2D(front, uv).rgb), 0.0, 10000000000.0);
+   float matte = texture2D(matte, uv).b;
+   col *= matte;
+   gl_FragColor = vec4(col, matte);
 }
