88d908ed35fe505677ec28a3fb87a01af25b4348
inferno-op <ivar@inferno-op.com>
2015-03-29 19:49:41 +0200

    fixed scaling center issue, added pixel aspect button
    - Added Pixel Aspect Correction button
    - Changed the way the scaling work to center

--- crok_pixelate/crok_pixelate.glsl
+++ crok_pixelate/crok_pixelate.glsl
@@ -1,17 +1,27 @@
 uniform sampler2D image;
 uniform sampler2D alpha;
-uniform float adsk_result_w, adsk_result_h;
+uniform float adsk_result_w, adsk_result_h, adsk_result_frameratio;
 uniform float Detail;
 uniform float trans;
+uniform bool Aspect;
 
-vec2 iResolution = vec2(adsk_result_w, adsk_result_h);
+vec2 resolution = vec2(adsk_result_w, adsk_result_h);
+
 
 void main(void)
 {
-	vec2 uv = gl_FragCoord.xy / iResolution.xy;
-	vec2 pixel = floor( uv * Detail );
-	vec3 color1 = vec3( texture2D(image, pixel / Detail ).rgb);	
+	vec2 uv = gl_FragCoord.xy / vec2( adsk_result_w, adsk_result_h );
+	
+	vec2 aspect = vec2(1.0);
+	
+	if ( Aspect )
+	    aspect = vec2(1.0, resolution.x/resolution.y);
+			
+    vec2 size = vec2(aspect.x/Detail, aspect.y/Detail);
+    vec2 pix_uv = uv - mod(uv - 0.5,size);
+
+	vec3 color1 = vec3( texture2D(image, pix_uv ).rgb);	
 	vec3 color2 = vec3( texture2D(image, uv).rgb);
-	vec3 matte =  vec3( texture2D(alpha, pixel / Detail).rgb);
+	vec3 matte =  vec3( texture2D(alpha, pix_uv).rgb);
 	gl_FragColor = vec4 ((matte * color1*trans + (1.0 - matte*trans) *color2) , matte);
 }
