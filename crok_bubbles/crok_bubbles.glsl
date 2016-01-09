3ae4f452d890106a3a7b2f2dc48d83b1c276569f
inferno-op <ivar@inferno-op.com>
2016-01-09 15:52:27 +0100

    hopefully fixed linux issue

--- crok_bubbles/crok_bubbles.glsl
+++ crok_bubbles/crok_bubbles.glsl
@@ -6,7 +6,7 @@ uniform float s_offset;
 uniform float Speed;
 uniform float Offset;
 uniform float Seed;
-uniform float Size;
+uniform float Radius;
 uniform float Sharpness;
 uniform float h_offset;
 uniform float v_offset;
@@ -32,13 +32,13 @@ void main(void)
 	{
         	// bubble seeds
 		float pha = sin(float(i)*5.13+1.0)*0.5 + 0.5;
-		float siz = pow( sin(float(i)*1.74+5.0)*0.5+ Seed+3., 4.0 );
+		float siz = pow( abs(sin(float(i)*1.74+5.0))*0.5+ Seed+3., 4.0 );
 		float pox = sin(float(i)*3.55+4.1) * resolution.x / resolution.y;
 
 		
         	// buble size, position and color
-		float rad = fRadius + sin(float(i))*0.12*s_offset+0.08*Size;
-		vec2  pos = vec2( pox+sin(time/30.*h_offset+pha+siz), -1.0-rad + (2.0+2.0*rad)*mod(pha+0.1*(time/5.*v_offset)*(0.2+0.8*Size),1.0)) * vec2(1.0, 1.0);
+		float rad = fRadius + sin(float(i))*0.12*s_offset+0.08*Radius ;
+		vec2  pos = vec2( pox+sin(time/30.*h_offset+pha+siz), -1.0-rad + (2.0+2.0*rad)*mod(pha+0.1*(time/5.*v_offset)*(0.2+0.8*Radius),1.0)) * vec2(1.0, 1.0);
 		float dis = length( uv - pos );
 		vec3  col = mix( vec3(tint.r, tint.g, tint.b), vec3(t_offset.r,t_offset.g,t_offset.b), 0.5+0.5*sin(float(i)*sin(time*pox*0.03)+1.9));
 		
