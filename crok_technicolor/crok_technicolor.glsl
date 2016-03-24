c720a68ea65a3e0db064d69de88064924ae634c3
inferno-op <ivar@inferno-op.com>
2016-03-24 19:41:24 +0100

    fixed White Frame bug under 2016 and up
    thx Lewis for finding the issue!

--- crok_technicolor/crok_technicolor.glsl
+++ crok_technicolor/crok_technicolor.glsl
@@ -4,77 +4,77 @@ vec2 resolution = vec2(adsk_result_w, adsk_result_h);
 uniform sampler2D Source;
 uniform float Exposure;
 uniform float Amount;
-uniform float Saturation;
+uniform float sat;
 uniform vec3 RGB_lum;
 uniform bool tc1, tc2, tc3, tc4;
 
 const vec3 lumcoeff = vec3(0.2126,0.7152,0.0722);
 
-const vec4 redfilter_tc1 		= vec4(1.0, 0.0, 0.0, 0.0);
-const vec4 bluegreenfilter_tc1 	= vec4(0.0, 1.0, 0.7, 0.0);
+const vec3 redfilter_tc1 		= vec3(1.0, 0.0, 0.0);
+const vec3 bluegreenfilter_tc1 	= vec3(0.0, 1.0, 0.7);
 
-const vec4 redfilter_tc2		= vec4(1.0, 0.0, 0.0, 0.0);
-const vec4 bluegreenfilter_tc2 	= vec4(0.0, 1.0, 1.0, 0.0);
-const vec4 cyanfilter_tc2		= vec4(0.0, 1.0, 0.5, 0.0);
-const vec4 magentafilter_tc2	= vec4(1.0, 0.0, 0.25, 0.0);
+const vec3 redfilter_tc2		= vec3(1.0, 0.0, 0.0);
+const vec3 bluegreenfilter_tc2 	= vec3(0.0, 1.0, 1.0);
+const vec3 cyanfilter_tc2		= vec3(0.0, 1.0, 0.5);
+const vec3 magentafilter_tc2	= vec3(1.0, 0.0, 0.25);
 
-const vec4 redfilter_tc3 		= vec4(1.0, 0.0, 0.0, 0.0);
-const vec4 greenfilter_tc3 		= vec4(0.0, 1.0, 0.0, 0.0);
-const vec4 bluefilter_tc3		= vec4(0.0, 0.0, 1.0, 0.0);
-const vec4 redorangefilter_tc3 	= vec4(.99, 0.263, 0.0, 0.0);
-const vec4 cyanfilter_tc3		= vec4(0.0, 1.0, 1.0, 0.0);
-const vec4 magentafilter_tc3	= vec4(1.0, 0.0, 1.0, 0.0);
-const vec4 yellowfilter_tc3 	= vec4(1.0, 1.0, 0.0, 0.0);
+const vec3 redfilter_tc3 		= vec3(1.0, 0.0, 0.0);
+const vec3 greenfilter_tc3 		= vec3(0.0, 1.0, 0.0);
+const vec3 bluefilter_tc3		= vec3(0.0, 0.0, 1.0);
+const vec3 redorangefilter_tc3 	= vec3(.99, 0.263, 0.0);
+const vec3 cyanfilter_tc3		= vec3(0.0, 1.0, 1.0);
+const vec3 magentafilter_tc3	= vec3(1.0, 0.0, 1.0);
+const vec3 yellowfilter_tc3 	= vec3(1.0, 1.0, 0.0);
 
 void main(void)
 {
 
 	vec2 uv = gl_FragCoord.xy / resolution.xy;
-	vec4 tc = texture2D(Source, uv);
+	vec3 tc = texture2D(Source, uv).rgb;
 
-	vec4 RGB_lum = vec4(lumcoeff * RGB_lum, 0.0 );
+	vec3 RGB_lum = vec3(lumcoeff * RGB_lum);
 	float lum = dot(tc,RGB_lum);
-	vec4 luma = vec4(lum);
+	vec3 luma = vec3(lum);
 	
-	vec4 col = vec4 (0.0, 0.0, 0.0, 0.0);
-	gl_FragColor = tc;
+	vec3 col = vec3(0.0);
+	//gl_FragColor = tc;
 		
 	if ( tc1 )
 	{
-	vec4 redrecord = tc * redfilter_tc1;
-	vec4 bluegreenrecord = tc * bluegreenfilter_tc1;
-	vec4 rednegative = vec4(redrecord.r);
-	vec4 bluegreennegative = vec4((bluegreenrecord.g + bluegreenrecord.b)/2.0);
-	vec4 redoutput = rednegative * redfilter_tc1;
-	vec4 bluegreenoutput = bluegreennegative * bluegreenfilter_tc1;
-	vec4 result = redoutput + bluegreenoutput;
+	vec3 redrecord = tc * redfilter_tc1;
+	vec3 bluegreenrecord = tc * bluegreenfilter_tc1;
+	vec3 rednegative = vec3(redrecord.r);
+	vec3 bluegreennegative = vec3((bluegreenrecord.g + bluegreenrecord.b)/2.0);
+	vec3 redoutput = rednegative * redfilter_tc1;
+	vec3 bluegreenoutput = bluegreennegative * bluegreenfilter_tc1;
+	vec3 result = redoutput + bluegreenoutput;
 	col = mix(tc, result, Amount);
 }
 
 	if ( tc2 )
 	{
-	vec4 redrecord = tc * redfilter_tc2;
-	vec4 bluegreenrecord = tc * bluegreenfilter_tc2;
-	vec4 rednegative = vec4(redrecord.r);
-	vec4 bluegreennegative = vec4((bluegreenrecord.g + bluegreenrecord.b)/2.0);
-	vec4 redoutput = rednegative + cyanfilter_tc2;
-	vec4 bluegreenoutput = bluegreennegative + magentafilter_tc2;
-	vec4 result = redoutput * bluegreenoutput;
+	vec3 redrecord = tc * redfilter_tc2;
+	vec3 bluegreenrecord = tc * bluegreenfilter_tc2;
+	vec3 rednegative = vec3(redrecord.r);
+	vec3 bluegreennegative = vec3((bluegreenrecord.g + bluegreenrecord.b)/2.0);
+	vec3 redoutput = rednegative + cyanfilter_tc2;
+	vec3 bluegreenoutput = bluegreennegative + magentafilter_tc2;
+	vec3 result = redoutput * bluegreenoutput;
 	col = mix(tc, result, Amount);
 }
 
 	if ( tc3 )
 	{
-	vec4 greenrecord = (tc) * greenfilter_tc3;
-	vec4 bluerecord = (tc) * magentafilter_tc3;
-	vec4 redrecord = (tc) * redorangefilter_tc3;
-	vec4 rednegative = vec4((redrecord.r + redrecord.g + redrecord.b)/3.0);
-	vec4 greennegative = vec4((greenrecord.r + greenrecord.g + greenrecord.b)/3.0);
-	vec4 bluenegative = vec4((bluerecord.r+ bluerecord.g + bluerecord.b)/3.0);
-	vec4 redoutput = rednegative + cyanfilter_tc3;
-	vec4 greenoutput = greennegative + magentafilter_tc3;
-	vec4 blueoutput = bluenegative + yellowfilter_tc3;
-	vec4 result = redoutput * greenoutput * blueoutput;
+	vec3 greenrecord = (tc) * greenfilter_tc3;
+	vec3 bluerecord = (tc) * magentafilter_tc3;
+	vec3 redrecord = (tc) * redorangefilter_tc3;
+	vec3 rednegative = vec3((redrecord.r + redrecord.g + redrecord.b)/3.0);
+	vec3 greennegative = vec3((greenrecord.r + greenrecord.g + greenrecord.b)/3.0);
+	vec3 bluenegative = vec3((bluerecord.r+ bluerecord.g + bluerecord.b)/3.0);
+	vec3 redoutput = rednegative + cyanfilter_tc3;
+	vec3 greenoutput = greennegative + magentafilter_tc3;
+	vec3 blueoutput = bluenegative + yellowfilter_tc3;
+	vec3 result = redoutput * greenoutput * blueoutput;
 	col = mix(tc, result, Amount);
 
 }
@@ -89,10 +89,10 @@ void main(void)
 	vec3 red =  greenmatte * bluematte * tc.r;
 	vec3 green = redmatte * bluematte * tc.g;
 	vec3 blue = redmatte * greenmatte * tc.b;
-	vec4 result = vec4(red.r, green.g, blue.b, tc.a);
+	vec3 result = vec3(red.r, green.g, blue.b);
 	col = mix(tc, result, Amount);
 
 }
-	gl_FragColor = mix(col, luma, Saturation) * Exposure;
+	gl_FragColor.rgb = mix(col, luma, sat) * Exposure;
 }
 
